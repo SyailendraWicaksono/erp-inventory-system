50ff5c3b669c38724ff27c4e4ed364b468a03142
Syailendra Julian Wicaksono <syailendra35wicaksono@gmail.com>
2026-08-01 15:03:12 +0700

    feat: create order and transaction models

--- backend/app/Models/Order.php
+++ backend/app/Models/Order.php
@@ -2,9 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'order_number',
+        'pickup_datetime',
+        'order_status',
+        'total_price',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
+
+    public function productionSchedule()
+    {
+        return $this->hasOne(ProductionSchedule::class);
+    }
 }
--- backend/app/Models/ProductionSchedule.php
+++ backend/app/Models/ProductionSchedule.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductionSchedule extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'start_time',
+        'end_time',
+        'production_status',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
